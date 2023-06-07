69ae44c45b8134a76343f17056e08f501aef9ac0
AdrianoLG-Medea <adriano.lorenzo@medea.es>
2023-06-07 11:39:39 +0200

    Ignore dist

--- dist/static/development/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/alumnos":["static\u002Fchunks\u002Fpages\u002Falumnos.js"],sortedPages:["\u002F_app","\u002F_error","\u002Falumnos"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
